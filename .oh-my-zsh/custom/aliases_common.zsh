4eef81098e5bf0eb1aae17f31b2b3b9d81958a92
Emre Can <emredjan@gmail.com>
2018-03-09 16:30:11 +0100

    zsh mac setup

---
+++ .oh-my-zsh/custom/aliases_common.zsh
@@ -0,0 +1,18 @@
+alias cuc="conda update conda"
+alias cua="conda update --all"
+alias cca="conda clean --all"
+
+alias clr="clear"
+
+alias cd..="cd .."
+alias ..="cd .."
+alias ...="cd ../.."
+
+alias jserve="bundle exec jekyll serve --watch --host 0.0.0.0"
+alias cb="conda activate base"
+alias cx="conda deactivate"
+alias jn="cb && jupyter notebook"
+alias jl="cb && jupyter lab"
+
+alias zshreload="source ~/.zshrc"
+
--- link.py
+++ link.py
@@ -10,18 +10,20 @@ dotdir = os.getcwd()
 
 dotfiles = [('.Rprofile', '.Rprofile'),
             ('.jupyter/custom/custom.css', '.jupyter/custom/custom.css'),
-            ('.config/nvim/init.vim', '.config/nvim/init.vim')]
-            
+            ('.config/nvim/init.vim', '.config/nvim/init.vim'),
+            ('.oh-my-zsh/custom/aliases_common.zsh', '.oh-my-zsh/custom/aliases_common.zsh')]
 
 if system == 'Darwin':
     dotfiles_add = [('.gitconfig_mac', '.gitconfig'),
                     ('.tmux.conf_mac', '.tmux.conf'),
-                    ('.config/fish/config.fish_mac', '.config/fish/config.fish')]
+                    ('.config/fish/config.fish_mac', '.config/fish/config.fish'),
+                    ('.zshrc_mac', '.zshrc'),
+                    ('.oh-my-zsh/custom/aliases_mac.zsh', '.oh-my-zsh/custom/aliases_mac.zsh')]
 elif system == 'Linux':
     dotfiles_add = [('.gitconfig_linux', '.gitconfig'),
                     ('.tmux.conf_linux', '.tmux.conf'),
                     ('.zshrc_linux', '.zshrc'),
-                    ('.oh-my-zsh/custom/aliases_linux.zsh', '.oh-my-zsh/custom/aliases.zsh')]
+                    ('.oh-my-zsh/custom/aliases_linux.zsh', '.oh-my-zsh/custom/aliases_linux.zsh')]
 
 dotfiles += dotfiles_add
 
