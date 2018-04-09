9e004ed93d89cae02f6ec55e19d45c3f8ed662ff
Emre Can <emredjan@gmail.com>
2018-04-09 23:45:36 +0200

    condarc

--- link.py
+++ link.py
@@ -11,7 +11,8 @@ dotdir = os.getcwd()
 dotfiles = [('.Rprofile', '.Rprofile'),
             ('.jupyter/custom/custom.css', '.jupyter/custom/custom.css'),
             ('.config/nvim/init.vim', '.config/nvim/init.vim'),
-            ('.oh-my-zsh/custom/aliases_common.zsh', '.oh-my-zsh/custom/aliases_common.zsh')]
+            ('.oh-my-zsh/custom/aliases_common.zsh', '.oh-my-zsh/custom/aliases_common.zsh'),
+            ('anaconda3/.condarc', 'anaconda3/.condarc')]
 
 if system == 'Darwin':
     dotfiles_add = [('.gitconfig_mac', '.gitconfig'),
